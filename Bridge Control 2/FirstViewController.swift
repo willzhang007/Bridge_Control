0b23a54f90d8bcf3d5f2a402d01a2c9c56c3548c
Lichao Zhang <lichaozhang@eduroamguest-2-186.sys.cs.local>
2016-10-24 17:50:14 -0400

    Initial Commit

---
+++ Bridge Control 2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Bridge Control 2
+//
+//  Created by Lichao Zhang on 10/24/16.
+//  Copyright © 2016 Lichao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
